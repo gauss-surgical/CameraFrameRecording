ffabfb10177e9a640c19d10c9cd4d850283e393d
Utsha Guha <utsha.guha@gausssurgical.com>
2020-08-25 15:17:01 -0700

    code clean up

--- GaussRecording/VideoCapture.swift
+++ GaussRecording/VideoCapture.swift
@@ -27,29 +27,14 @@ public class VideoCapture: NSObject, AVCapturePhotoCaptureDelegate {
     
     var photoOutput = AVCapturePhotoOutput()
     
-//    public func setUp(sessionPreset: AVCaptureSession.Preset = .photo,
-//                      completion: @escaping (Bool) -> Void) {
-//        queue.async {
-//            let success = self.setUpCamera(sessionPreset: sessionPreset)
-//            DispatchQueue.main.async {
-//                completion(success)
-//            }
-//        }
-//    }
-    
     func setUpCamera(sessionPreset: AVCaptureSession.Preset, completion: @escaping (AVCaptureVideoPreviewLayer?) -> Void) {
         captureSession.beginConfiguration()
         defer { captureSession.commitConfiguration() }
-        
-//        guard let captureDevice = AVCaptureDevice.default(for: AVMediaType.video) else {
-//            print("Error: no video devices available")
-//            completion(nil)
-//        }
-        
+                
         if AVCaptureDevice.default(for: AVMediaType.video) == nil {
             completion(nil)
         }
-        //let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)!
+
         let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .front)!
         
         let exposureDuration = captureDevice.exposureDuration
@@ -61,22 +46,6 @@ public class VideoCapture: NSObject, AVCapturePhotoCaptureDelegate {
         captureDevice.setExposureModeCustom(duration: exposureDuration, iso: iso, completionHandler: nil)
         captureDevice.unlockForConfiguration()
         
-//        if(captureDevice.isFocusModeSupported(.continuousAutoFocus)) {
-//            try! captureDevice.lockForConfiguration()
-//            captureDevice.focusMode = .continuousAutoFocus
-//            captureDevice.unlockForConfiguration()
-//        }
-//        
-//        if(captureDevice.isWhiteBalanceModeSupported(.autoWhiteBalance)) {
-//            try! captureDevice.lockForConfiguration()
-//            captureDevice.whiteBalanceMode = .autoWhiteBalance
-//            captureDevice.unlockForConfiguration()
-//        }
-//
-//        try! captureDevice.lockForConfiguration()
-//        captureDevice.exposureMode = .continuousAutoExposure
-//        captureDevice.unlockForConfiguration()
-
         if let videoInput = try? AVCaptureDeviceInput(device: captureDevice) {
             if captureSession.canAddInput(videoInput) {
                 captureSession.addInput(videoInput)
@@ -135,7 +104,6 @@ public class VideoCapture: NSObject, AVCapturePhotoCaptureDelegate {
         if !captureSession.isRunning {
             queue.async {
                 self.captureSession.startRunning()
-                //self.toggleTorch(on: true)
             }
         }
     }
@@ -146,74 +114,9 @@ public class VideoCapture: NSObject, AVCapturePhotoCaptureDelegate {
         if captureSession.isRunning {
             queue.async {
                 self.captureSession.stopRunning()
-                //self.toggleTorch(on: false)
             }
         }
     }
-    
-//    func toggleTorch(on: Bool) {
-//        if appMode == .GlobalGood_Provider || appMode == .USC_Provider || appMode == .Cellex_Provider {
-//            return
-//
-//        }
-//        guard
-//            let device = AVCaptureDevice.default(for: AVMediaType.video),
-//            device.hasTorch
-//        else { return }
-//
-//        do {
-//            try device.lockForConfiguration()
-//            try device.setTorchModeOn(level:1.0)
-//            device.torchMode = on ? .on : .off
-//            device.unlockForConfiguration()
-//        } catch {
-//            print("Torch could not be used")
-//        }
-//    }
-    
-//    public func capturePhoto(completion: @escaping ()->()) {
-//        toggleTorch(on: false)
-//            let pixelFormatType = kCVPixelFormatType_32BGRA
-//            guard self.photoOutput.availablePhotoPixelFormatTypes.contains(pixelFormatType) else { return }
-//            let photoSettings = AVCapturePhotoSettings(format:
-//                [ kCVPixelBufferPixelFormatTypeKey as String : pixelFormatType ])
-//        if appMode == .GlobalGood_Provider || appMode == .Cellex_Provider{
-//            if self.photoOutput.supportedFlashModes.contains(.on){
-//                photoSettings.flashMode = .on
-//            }
-//
-//        }
-//            // Shoot the photo, using a custom class to handle capture delegate callbacks.
-//            self.photoOutput.capturePhoto(with: photoSettings, delegate: self)
-//            completion()
-//        //}
-//    }
-    
-//    public func capturePhotoWith(flashOn:Bool, completion: @escaping ()->()) {
-//        toggleTorch(on: false)
-//        //DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-//            let pixelFormatType = kCVPixelFormatType_32BGRA
-//            guard self.photoOutput.availablePhotoPixelFormatTypes.contains(pixelFormatType) else { return }
-//            let photoSettings = AVCapturePhotoSettings(format:
-//                [ kCVPixelBufferPixelFormatTypeKey as String : pixelFormatType ])
-//        if appMode == .GlobalGood_Provider || appMode == .Cellex_Provider{
-//            if self.photoOutput.supportedFlashModes.contains(.on){
-//                photoSettings.flashMode = ((flashOn == true) ? .on : .off)
-//            }
-//
-//        }
-//            // Shoot the photo, using a custom class to handle capture delegate callbacks.
-//            self.photoOutput.capturePhoto(with: photoSettings, delegate: self)
-//            completion()
-//        //}
-//    }
-        
-    // Hold on to the separately delivered RAW file and compressed photo data until capture is finished.
-//    public func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-//        // UNCOMPRESSED
-//       // toggleTorch(on: true)
-//        photoDelegate?.rawPhotoCapture(rawPhoto: photo)
-//    }
 }
 
 extension VideoCapture: AVCaptureVideoDataOutputSampleBufferDelegate {
